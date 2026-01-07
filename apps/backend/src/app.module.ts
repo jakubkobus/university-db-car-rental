f8c49f42f06f8f97d170d5805ef4c918ac6e937d
Jakub Kobus <jkb.kobus@gmail.com>
2026-01-07 23:53:11 +0100

    feat(CRUD): all needed resources

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CarsModule } from './cars/cars.module';
+import { CategoriesModule } from './categories/categories.module';
+import { FeaturesModule } from './features/features.module';
+import { UsersModule } from './users/users.module';
+import { RentalsModule } from './rentals/rentals.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { MaintenanceModule } from './maintenance/maintenance.module';
 
 @Module({
-  imports: [PrismaModule, CarsModule],
+  imports: [PrismaModule, CarsModule, CategoriesModule, FeaturesModule, UsersModule, RentalsModule, ReviewsModule, MaintenanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
